0d1384fe7399f5b5a748cc773cc270b7180e5ec2
Fabian <238008@student.pwr.edu.pl>
2020-04-29 21:33:50 +0200

    Init commit.

---
+++ src/main/java/com/piisw/cinema_tickets_app/CinemaTicketsAppApplication.java
@@ -0,0 +1,13 @@
+package com.piisw.cinema_tickets_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piisw/cinema_tickets_app/CinemaTicketsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piisw.cinema_tickets_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
