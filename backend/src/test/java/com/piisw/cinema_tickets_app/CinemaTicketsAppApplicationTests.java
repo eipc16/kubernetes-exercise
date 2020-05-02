83a41116440a716b61196d32dbccc5ab91970abc
MatCoder77 <matkimzag@wp.pl>
2020-05-02 16:50:29 +0200

    Changed circleci config

--- backend/src/test/java/com/piisw/cinema_tickets_app/CinemaTicketsAppApplicationTests.java
+++ backend/src/test/java/com/piisw/cinema_tickets_app/CinemaTicketsAppApplicationTests.java
@@ -1,13 +1,8 @@
 package com.piisw.cinema_tickets_app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CinemaTicketsAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
