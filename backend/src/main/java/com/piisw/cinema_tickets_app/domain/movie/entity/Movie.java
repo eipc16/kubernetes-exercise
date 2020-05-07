5298c5f380bfbb302d7be0166c3dc9a37cbede20
MatCoder77 <matkimzag@wp.pl>
2020-05-07 22:37:27 +0200

    Enabled jpa metamodel generation

---
+++ backend/src/main/java/com/piisw/cinema_tickets_app/domain/movie/entity/Movie.java
@@ -0,0 +1,16 @@
+package com.piisw.cinema_tickets_app.domain.movie.entity;
+
+import com.piisw.cinema_tickets_app.domain.auditedobject.AuditedObject;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Movie extends AuditedObject {
+
+    @Id
+    @GeneratedValue(generator = "ID_GENERATOR")
+    private Long id;
+
+}
---
+++ backend/src/main/java/com/piisw/cinema_tickets_app/package-info.java
@@ -0,0 +1,9 @@
+@org.hibernate.annotations.GenericGenerator(
+        name = "ID_GENERATOR",
+        strategy = "enhanced-sequence",
+        parameters = {
+                @org.hibernate.annotations.Parameter(name = "sequence_name", value = "GENERAL_SEQUENCE"),
+                @org.hibernate.annotations.Parameter(name = "initial_value", value = "1")
+        }
+)
+package com.piisw.cinema_tickets_app;
