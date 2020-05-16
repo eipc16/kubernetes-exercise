a48a1fc9ecd112aa3a2260e02d84dc237ec31e42
Przemysław Pietrzak <Przemyslaw.Pietrzak@comarch.com>
2020-05-17 00:03:54 +0200

    fix build

--- ui/src/main/app/utils/custom-fetch-hook.ts
+++ ui/src/main/app/utils/custom-fetch-hook.ts
@@ -6,5 +6,6 @@ export const useFetching = (actionCreator: (dispatch: Dispatch<Action>) => void)
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(actionCreator);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-}
+};
