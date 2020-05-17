9f3de3859f3be4c19f2d49dd77e0b527e37cc084
MatCoder77 <matkimzag@wp.pl>
2020-05-17 22:34:28 +0200

    MovieService refactor

--- backend/src/main/java/com/piisw/cinema_tickets_app/domain/movie/control/MovieService.java
+++ backend/src/main/java/com/piisw/cinema_tickets_app/domain/movie/control/MovieService.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.ZoneOffset;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
