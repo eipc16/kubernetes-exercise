6f8dc193b89804ed3399a73e988a13af1d9393d7
Fabian <238008@student.pwr.edu.pl>
2020-06-06 16:28:02 +0200

    Reservation submit modal added.

--- ui/src/main/app/components/reservation-page/reservation-actions/reservation-actions.tsx
+++ ui/src/main/app/components/reservation-page/reservation-actions/reservation-actions.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 
 import './reservation-actions.scss'
-import {Button} from "antd";
+import {Button, Modal} from "antd";
 import {connect} from "react-redux";
 import {SeatActionPublisher, SeatsMap} from "../../../redux/actions/seat";
 import {ReservationState} from "../../../models/screening-rooms";
@@ -21,8 +21,49 @@ const ReservationActionsComponent = (props: ReservationActionsProps) => {
     let { className, seats } = props;
     let counter = Object.values(seats).filter(seat => seat.reservationState === ReservationState.SELECTED).length
 
+
+    const [modalState, setModalState] = useState({loading: false, visible: false})
+
+    const showModal = () => {
+        if (counter > 0) {
+            setModalState({...modalState, visible: true});
+        }
+    };
+
+    const handleOk = () => {
+        setModalState({ ...modalState, loading: true });
+        setTimeout(() => {
+            setModalState({ loading: false, visible: false });
+        }, 3000);
+    };
+
+    const handleCancel = () => {
+        setModalState({ ...modalState, visible: false });
+    };
+
     return (
         <div className={className} >
+            <Modal
+                visible={modalState.visible}
+                title="Submit reservation"
+                onOk={showModal}
+                onCancel={handleCancel}
+                footer={[
+                    <Button key="back" onClick={handleCancel}>
+                        Return
+                    </Button>,
+                    <Button key="submit" type="primary" loading={modalState.loading} onClick={handleOk}>
+                        Submit
+                    </Button>,
+                ]}
+            >
+                <p>
+                    Number of seats: {counter}
+                </p>
+                <p>
+                    Total cost: {counter * 10} $
+                </p>
+            </Modal>
             <span>
                 Your reservation:
             </span>
@@ -32,7 +73,7 @@ const ReservationActionsComponent = (props: ReservationActionsProps) => {
             <span>
                 Total cost: {counter * 10} $
             </span>
-            <Button>
+            <Button onClick={showModal} disabled={counter < 1}>
                 Reserve
             </Button>
         </div>
